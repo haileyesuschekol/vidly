4819d787af2957fe0ecdba30b0e9892d97428d21
Haileyesus Chekol <teamdevs7@gmail.com>
2024-10-12 16:50:14 +0100

    make post delete and put requests

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express")
-
+const Joi = require("joi")
 const app = express()
 app.use(express.json())
 
@@ -26,9 +26,41 @@ app.get("/api/genres/:id", (req, res) => {
   }
 })
 
-// app.post('/api/genres', (req,res)=>{
+app.post("/api/genres", (req, res) => {
+  validateCourse(req.body)
+  const newGenres = { id: genres.length + 1, name: req.body.name }
+  genres.push(newGenres)
+  res.send(genres).status(201)
+})
 
-// })
+app.put("/api/genres/:id", (req, res) => {
+  const finder = genres.find((g) => g.id === parseInt(req.params.id))
+  if (!finder) {
+    res.send("not found").status(400)
+  }
+  validateCourse(req.body)
+  finder.name = req.body.name
+  res.send(finder).status(201)
+})
+
+app.delete("/api/genres/:id", (req, res) => {
+  const finder = genres.find((g) => g.id === parseInt(req.params.id))
+  if (!finder) {
+    return res.send("invalid request").status(400)
+  } else {
+    const index = genres.indexOf(finder)
+    genres.splice(index, 1)
+    return res.send(genres).status(200)
+  }
+})
+
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().max(20).min(1).required(),
+  })
+  const result = schema.validate(course)
+  if (result.error) return res.send(result.error.details[0].message).status(400)
+}
 
 app.listen(3000, () => {
   console.log("listen in port 3000")
